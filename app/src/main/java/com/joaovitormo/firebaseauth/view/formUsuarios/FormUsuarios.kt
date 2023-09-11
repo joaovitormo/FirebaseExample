7cce478b09950737c782abe8f542ce74f9316dee
João Vitor <vitormendes388@gmail.com>
2023-09-10 21:53:46 -0300

    New layout with RecyclerView

---
+++ app/src/main/java/com/joaovitormo/firebaseauth/view/formUsuarios/FormUsuarios.kt
@@ -0,0 +1,12 @@
+package com.joaovitormo.firebaseauth.view.formUsuarios
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.joaovitormo.firebaseauth.R
+
+class FormUsuarios : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_form_usuarios)
+    }
+}
